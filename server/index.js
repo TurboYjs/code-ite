c7e0528fec265a0ed8135272e75403e513aa1156
guxukai <gxk@33.cn>
2023-09-14 14:47:47 +0800

    chore: release v0.0.16

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const io = require("socket.io")(server, {
   },
   pingTimeout: 60000,
   upgradeTimeout: 30000,
-  path: '/socket.io/'
+  path: '/socket/'
 });
 require("./socket/socketEditorAdapter")(io);
 
